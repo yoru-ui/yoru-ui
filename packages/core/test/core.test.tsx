f6149cbcfc95813e332bf9000f36c6731fdd1ee6
Leonard Monosa <54368620+leonardmns12@users.noreply.github.com>
2022-08-12 19:06:49 +0700

    Feat/yoru-core (#9)
    Co-authored-by: Leonard <leonard.monosa@gambitstocks.com>

--- packages/core/test/core.test.tsx
+++ packages/core/test/core.test.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import { render, screen } from '@testing-library/react';
 import { yoru } from '../index';
 
-describe('Should test the system factory', () => {
-  it('Should test the system factory', () => {
+describe('Should test the factory system', () => {
+  it('Should test the factory system', () => {
     render(<yoru.button className="yoru-button">Yoru Button</yoru.button>);
     expect(screen.getByText('Yoru Button').className).toContain('yoru-button');
   });
