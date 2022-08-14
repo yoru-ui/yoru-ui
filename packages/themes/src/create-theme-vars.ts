a3a5e99fc90eb073444fe8e93cc267102a9428ad
Muhammad Irva <muhammad.irva@bibit.id>
2022-08-14 13:41:00 +0700

    fix import error from yoru-ui/system to yoru-ui/core

--- packages/themes/src/create-theme-vars.ts
+++ packages/themes/src/create-theme-vars.ts
@@ -1,4 +1,4 @@
-import { flattenTokens } from '@yoru-ui/system';
+import { flattenTokens } from '@yoru-ui/core';
 
 export function yoruCSSVars(rawTheme: Record<string, any>): Record<string, any> {
   const theme = {};
