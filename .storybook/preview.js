b7e0ec405bee487015f08392be5a20b1feef4f8a
Muhammad Irva <muhammad.irva@bibit.id>
2022-08-26 09:47:11 +0700

    setup storybook decarator

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,26 @@
+import { YoruProvider } from '@yoru-ui/themes';
+
+export const decorators = [
+  Story => (
+    <YoruProvider
+      theme={{
+        colors: {
+          primary: '#fac',
+        },
+      }}
+      config={{ initialColorMode: 'light', useSystemColorMode: true }}
+    >
+      <Story />
+    </YoruProvider>
+  ),
+];
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
